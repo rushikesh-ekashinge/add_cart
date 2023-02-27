1f8530276940a0983c5a55bd58a9ef7ad919a894
Rushikesh Ekashinge <100825683+rushikesh-ekashinge@users.noreply.github.com>
2023-02-27 17:52:03 +0530

    Add files via upload

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getProduct() {
+    return this.http.get<any>("https://fakestoreapi.com/products")
+      .pipe(map((res: any) => {
+        return res;
+      }))
+  }
+}
